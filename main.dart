4ddd8944f144ebe5868e6f7ffea39a3705c5c293
Titan <68808479+titan3755@users.noreply.github.com>
2022-05-25 13:02:21 +0600

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,62 @@
+// Simple calcuator app
+
+import "dart:io";
+
+void main() {
+  while (true) {
+    calc();
+    print("Enter to continue or type \"exit\" to exit the app...");
+    String? userResponse = stdin.readLineSync();
+    if (userResponse == "exit") {
+      break;
+    } else {
+      continue;
+    }
+  }
+}
+
+void calc() {
+  print("Enter two numbers separated by space: ");
+  String? numbers = stdin.readLineSync();
+  if (numbers == null) {
+    print("Invalid input");
+    return;
+  } else {
+    List<String>? nums = numbers.split(" ");
+    if (nums.length != 2) {
+      print("Invalid input");
+      return;
+    } else {
+      int? num1 = int.tryParse(nums[0]);
+      int? num2 = int.tryParse(nums[1]);
+      if (num1 == null || num2 == null) {
+        print("Invalid input");
+        return;
+      } else {
+        print("Operator between these numbers: ");
+        String? operation = stdin.readLineSync();
+        if (operation == null) {
+          print("Invalid input");
+          return;
+        } else {
+          switch (operation) {
+            case "+":
+              print("Sum: ${num1 + num2}");
+              break;
+            case "-":
+              print("Difference: ${num1 - num2}");
+              break;
+            case "*":
+              print("Product: ${num1 * num2}");
+              break;
+            case "/":
+              print("Quotient: ${num1 / num2}");
+              break;
+            default:
+              print("Invalid input");
+          }
+        }
+      }
+    }
+  }
+}
